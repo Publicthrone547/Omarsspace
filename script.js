a7441da09cb4f782d193baa7de459376c544738d
Publicthrone547 <165716978+Publicthrone547@users.noreply.github.com>
2024-04-14 23:26:36 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const video = document.getElementById('video');
+    const playpause = document.getElementById('playpause');
+    const seek = document.getElementById('seek');
+    
+    playpause.addEventListener('click', function() {
+      if (video.paused || video.ended) {
+        video.play();
+      } else {
+        video.pause();
+      }
+    });
+  
+    video.addEventListener('play', function() {
+      playpause.textContent = 'Pause';
+    });
+  
+    video.addEventListener('pause', function() {
+      playpause.textContent = 'Play';
+    });
+  
+    video.addEventListener('timeupdate', function() {
+      seek.value = video.currentTime;
+    });
+  
+    seek.addEventListener('input', function() {
+      video.currentTime = seek.value;
+    });
+  });
