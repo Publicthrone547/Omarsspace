40916c514a2c940bc7dbf4c4ce063dffdaba2d4f
Publicthrone547 <165716978+Publicthrone547@users.noreply.github.com>
2024-04-22 21:14:18 +0500

    Update script.js

--- script.js
+++ script.js
@@ -22,8 +22,26 @@ document.addEventListener('DOMContentLoaded', function() {
     video.addEventListener('timeupdate', function() {
       seek.value = video.currentTime;
     });
-  
+    
     seek.addEventListener('input', function() {
       video.currentTime = seek.value;
     });
   });
+function sendNotification() {
+    if (Notification.permission === 'granted') {
+        var notification = new Notification('Public547', {
+            body: 'Это тест отправляется ли сообшение,АУФ'
+        });
+    } else if (Notification.permission !== 'denied') {
+        Notification.requestPermission().then(function(permission) {
+            if (permission === 'granted') {
+                var notification = new Notification('Поч не разрешил', {
+                    body: 'Какашка'
+                });
+            }
+        });
+    }
+}
+
+// Вызываем функцию отправки уведомления каждые 30 секунд
+setInterval(sendNotification, 30000); // 30000 миллисекунд = 30 секунд
